e513f299a64c184bf04ec5caed9b2511081cbcd7
TT-struggleofoneself <504470358@qq.com>
2016-04-14 12:04:40 +0800

    Initial Commit

---
+++ APPWatchFunctionList WatchKit Extension/ExtensionDelegate.h
@@ -0,0 +1,13 @@
+//
+//  ExtensionDelegate.h
+//  APPWatchFunctionList WatchKit Extension
+//
+//  Created by TT_code on 16/4/14.
+//  Copyright © 2016年 TT_code. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+@interface ExtensionDelegate : NSObject <WKExtensionDelegate>
+
+@end
---
+++ APPWatchFunctionList WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  APPWatchFunctionList WatchKit Extension
+//
+//  Created by TT_code on 16/4/14.
+//  Copyright © 2016年 TT_code. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
